ab8ec2cbae9a1540159437ab1675aa3a2dbab96f
Amazonian32 <ynitsedekralc@gmail.com>
2020-01-10 01:30:27 -0700

    I made varibles for each button, and was able to assign a randomly generated value anytime the page loads. I console.logged it to make sure the values were correctly wired to the buttons, and they were

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,19 @@
-$(document).ready(function(){
-    var randomNumber = Math.floor((Math.random() *120) + 19);
+$(document).ready(function () {
+    var randomNumber = Math.floor((Math.random() * 120) + 19);
     $("#random-number").text(randomNumber);
-    console.log(randomNumber);
-    $("button").on("click", function() {
-        console.log("clicked: " + $(this).attr("id"));
+
+    var diamond = Math.floor((Math.random() * 12) + 1);
+    var emerald = Math.floor((Math.random() * 12) + 1);
+    var ruby = Math.floor((Math.random() * 12) + 1);
+    var topaz = Math.floor((Math.random() * 12) + 1);
+    $("#diamond").val(diamond);
+    $("#emerald").val(emerald);
+    $("#ruby").val(ruby);
+    $("#topaz").val(topaz);
+    console.log(randomNumber, diamond, emerald, ruby, topaz);
+    $("button").on("click", function () {
+        console.log("clicked: " + $(this).attr("id") + " " + $(this).attr("value"));
     })
+
+
 })
