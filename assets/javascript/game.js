0574ff79e8b4154b6176e46325b97d17ffe7979a
Amazonian32 <ynitsedekralc@gmail.com>
2020-01-10 01:18:37 -0700

    I got the script to generate a random number everytime the document loads between 19-120, console.log it, and display it on the screen!!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    var randomNumber = Math.floor((Math.random() *120) + 19);
+    $("#random-number").text(randomNumber);
+    console.log(randomNumber);
     $("button").on("click", function() {
         console.log("clicked: " + $(this).attr("id"));
     })
